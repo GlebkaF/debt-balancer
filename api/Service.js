b7b7dae5671ce5ac2f3b85ff48a8b2bf11951253
GlebkaF <1fokingleb@gmail.com>
2019-11-12 13:51:51 +0700

    Реализует функционал погащения долго и просмотра информации о юзере

--- api/Service.js
+++ api/Service.js
@@ -1,33 +1,76 @@
 const _ = require('lodash')
 
+const updateBalance = balanceUpdate => currentBalance => (currentBalance || 0) + balanceUpdate;
+
+class ServiceError extends Error { }
+
 class Service {
     constructor({ users }) {
+        // Если баланс юзера положительный, значит ему должны
+        // Если отрицательный, значит он должен
         this.users = users;
     }
+
     /**
      * Добавляет общую покупку нa price рублей, от имени buyerId
      * стоимость покупки поделится равными частями между пользователями usersIds
      * 
-     * @param {string} buyerId 
-     * @param {number} price - сумма в рублях 
-     * @param {Array<string>} usersIds 
+     * @param {Object} params
+     * @param {string} params.buyerId - покупатель
+     * @param {number} params.price - сумма в рублях 
+     * @param {Array<string>} params.usersIds - для кого предназначается покупка, может как влючать покупателя, так и не включать
      */
     addPurchase({ buyerId, price, usersIds }) {
+        if(!buyerId || !price || !usersIds) {
+            throw new ServiceError('Не переданы обязательные параметры')
+        }
+
+        if (price < 0) {
+            throw new ServiceError('Покупка не может быть на отрицательную сумму')
+        }
+
         const debt = Math.round(price / usersIds.length);
 
         // Убираем покупателя из списка должников, т.к. сам себе должен он быть не может
         const debtorsIds = _.without(usersIds, buyerId);
 
         debtorsIds.forEach((debtorId) => {
-            const updateBalance = debt => balance => (balance || 0) + debt;
-
             _.update(this.users, [debtorId, 'balances', buyerId], updateBalance(-debt));
             _.update(this.users, [buyerId, 'balances', debtorId], updateBalance(debt));
         });
-
     }
 
-    payDebt() { }
+    /**
+     * Гасит задолженность
+     * @param {Object} params
+     * @param {string} params.debtorId - должник, тот кто гасит долг
+     * @param {string} params.creditorId - кредитор, тот кому должны
+     * @param {string} params.amount - на сколько рублей гасим долг
+     */
+    payDebt({ debtorId, creditorId, amount }) {
+        if(!debtorId || !creditorId || !amount) {
+            throw new ServiceError('Не переданы обязательные параметры')
+        }
+
+        if (debtorId === creditorId) {
+            throw new ServiceError('Нельзя погасить долг самому себе')
+        }
+
+        if (amount < 0) {
+            throw new ServiceError('Нельзя погасить долг на отрицательную сумму')
+        }        
+
+        _.update(this.users, [debtorId, 'balances', creditorId], updateBalance(-amount));
+        _.update(this.users, [creditorId, 'balances', debtorId], updateBalance(amount));
+    }
+
+    /**
+     * Получает информацию о юзере по id
+     * @param {string} userId 
+     */
+    getUserInfo({ userId }) {
+        return this.users[userId];
+    }
 }
 
 module.exports = Service;
--- api/Service.spec.js
+++ api/Service.spec.js
@@ -11,63 +11,193 @@ describe('Service', () => {
         expect(service).toBeTruthy()
     })
 
-    it('Должен создавать балансы между юзерами если балансов между юзерами еще нет', () => {
-        const users = {
-            [LERA]: {
-                balance: {},
-            },
-            [GLEB]:{
-                balance: {},
-            },
-            [ANTON]: {
-                balance: {}
-            }
-        }
-        const service = new Service({ users });
-
-        service.addPurchase({
-            buyerId: GLEB,
-            price: 600,
-            usersIds: [
-                GLEB,
-                ANTON
-            ]
-        })        
-
-        expect(service.users[GLEB].balances[ANTON]).toBe(300);
-        expect(service.users[ANTON].balances[GLEB]).toBe(-300);
-        expect(service.users[GLEB].balances[GLEB]).toBeUndefined();
-    })
-    
-    it('Должен обновлять балансы между юзерами если между юзерами уже есть баланс', () => {
-        const users = {
-            [LERA]: {
-                balances: {},
-            },
-            [GLEB]:{
-                balances: {
-                    [ANTON]: -100
+    describe('Добавление общей покупки', () => {
+        it('Должен создавать балансы между юзерами если балансов между юзерами еще нет', () => {
+            const users = {
+                [LERA]: {
+                    balance: {},
                 },
-            },
-            [ANTON]: {
-                balances: {
-                    [GLEB]: 100
+                [GLEB]: {
+                    balance: {},
+                },
+                [ANTON]: {
+                    balance: {}
                 }
             }
-        }
-        const service = new Service({ users });
+            const service = new Service({ users });
 
-        service.addPurchase({
-            buyerId: GLEB,
-            price: 600,
-            usersIds: [
-                GLEB,
-                ANTON
-            ]
-        })        
+            service.addPurchase({
+                buyerId: GLEB,
+                price: 600,
+                usersIds: [
+                    GLEB,
+                    ANTON
+                ]
+            })
 
-        expect(service.users[GLEB].balances[ANTON]).toBe(200);
-        expect(service.users[ANTON].balances[GLEB]).toBe(-200);
-        expect(service.users[GLEB].balances[GLEB]).toBeUndefined();
-    })  
+            expect(service.users[GLEB].balances[ANTON]).toBe(300);
+            expect(service.users[ANTON].balances[GLEB]).toBe(-300);
+            expect(service.users[GLEB].balances[GLEB]).toBeUndefined();
+        })
+
+        it('Должен обновлять балансы между юзерами если между юзерами уже есть баланс', () => {
+            const users = {
+                [LERA]: {
+                    balances: {},
+                },
+                [GLEB]: {
+                    balances: {
+                        [ANTON]: -100
+                    },
+                },
+                [ANTON]: {
+                    balances: {
+                        [GLEB]: 100
+                    }
+                }
+            }
+            const service = new Service({ users });
+
+            service.addPurchase({
+                buyerId: GLEB,
+                price: 600,
+                usersIds: [
+                    GLEB,
+                    ANTON
+                ]
+            })
+
+            expect(service.users[GLEB].balances[ANTON]).toBe(200);
+            expect(service.users[ANTON].balances[GLEB]).toBe(-200);
+            expect(service.users[GLEB].balances[GLEB]).toBeUndefined();
+        })
+
+        it('Должен обновлять балансы если во владельцах покупки нет покупателя', () => {
+            const users = {
+                [LERA]: {
+                    balances: {},
+                },
+                [GLEB]: {
+                    balances: {
+                        [ANTON]: -100
+                    },
+                },
+                [ANTON]: {
+                    balances: {
+                        [GLEB]: 100
+                    }
+                }
+            }
+            const service = new Service({ users });
+
+            service.addPurchase({
+                buyerId: GLEB,
+                price: 600,
+                usersIds: [
+                    LERA,
+                    ANTON
+                ]
+            })
+
+            expect(service.users[GLEB].balances[ANTON]).toBe(200);
+            expect(service.users[ANTON].balances[GLEB]).toBe(-200);
+            expect(service.users[GLEB].balances[LERA]).toBe(300);
+            expect(service.users[LERA].balances[GLEB]).toBe(-300);
+            expect(service.users[GLEB].balances[GLEB]).toBeUndefined();
+        })
+    })
+
+    describe('Погашение долга', () => {
+        it('Должен гасить долг полностью', () => {
+            const users = {
+                [LERA]: {
+                    balances: {},
+                },
+                [GLEB]: {
+                    balances: {
+                        [ANTON]: -100
+                    },
+                },
+                [ANTON]: {
+                    balances: {
+                        [GLEB]: 100
+                    }
+                }
+            }
+
+            const service = new Service({ users });
+
+            service.payDebt({
+                debtorId: ANTON,
+                creditorId: GLEB,
+                amount: 100,                
+            })
+
+            expect(service.users[GLEB].balances[ANTON]).toBe(0);
+            expect(service.users[ANTON].balances[GLEB]).toBe(0);
+        })
+
+        it('Должен гасить долг частично', () => {
+            const users = {
+                [LERA]: {
+                    balances: {},
+                },
+                [GLEB]: {
+                    balances: {
+                        [ANTON]: -100
+                    },
+                },
+                [ANTON]: {
+                    balances: {
+                        [GLEB]: 100
+                    }
+                }
+            }
+
+            const service = new Service({ users });
+
+            service.payDebt({
+                debtorId: ANTON,
+                creditorId: GLEB,
+                amount: 50,                
+            })
+
+            expect(service.users[GLEB].balances[ANTON]).toBe(-50);
+            expect(service.users[ANTON].balances[GLEB]).toBe(50);
+        })
+    })
+
+    describe('Просмотр информации о балансе юзера', () => {
+        it('Должен выводить баланс существующего юзера', ()=> {
+            const users = {
+                [LERA]: {
+                    balances: {
+                        [GLEB]: -3000,
+                        [ANTON]: -200,
+                    },
+                },
+                [GLEB]: {
+                    balances: {
+                        [ANTON]: -100,
+                        [LERA]: 3000,
+                    },
+                },
+                [ANTON]: {
+                    balances: {
+                        [GLEB]: 100,
+                        [LERA]: 200,
+                    }
+                }
+            }
+
+            const service = new Service({ users });
+
+            const gleb = service.getUserInfo({ userId: GLEB});
+
+            expect(gleb.balances).toEqual({
+                [ANTON]: -100,
+                [LERA]: 3000,
+            },);
+        })
+    })
 })
