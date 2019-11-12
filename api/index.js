9491c6c471ed3efc83ebe9d55ec39befae1afe83
GlebkaF <1fokingleb@gmail.com>
2019-11-12 16:56:07 +0700

    Подключает тестовый endpoint к реальной монге

--- api/index.js
+++ api/index.js
@@ -1,3 +1,23 @@
-module.exports = async (req, res) => {    
-    res.end(`MONGODB_URI2: ${process.env.MONGODB_URI}`)
+const MongoClient = require('mongodb').MongoClient;
+
+const Service = require('./Service');
+
+module.exports = async (req, res) => {
+    try {
+      const id = req.query.id;
+      const db = await connectToMongo();
+      const service = new Service({ db});
+      const userInfo = await service.getUserInfo({ id: 'glebkaf'})      
+      res.json(userInfo);
+    } catch(error) {
+      res.send(error);  
+    }
+}
+
+async function connectToMongo() {
+  const uri = process.env.MONGODB_URI;
+  const client = new MongoClient(uri, { useNewUrlParser: true });
+  const connection = await client.connect()
+  const db = connection.db('debt-balancer');  
+  return db;
 }
