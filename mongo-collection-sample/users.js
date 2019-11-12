f52e5c29d80087164c5d52f81e9258cd65d1ed26
GlebkaF <1fokingleb@gmail.com>
2019-11-12 20:27:33 +0700

    Добавляет пример списка юзеров для коллекции users

---
+++ mongo-collection-sample/users.js
@@ -0,0 +1,10 @@
+db.users.insertMany([
+    { id: 'Devkabezruki', balances: {}, name: 'Лера' },
+    { id: 'glebkaf', balances: {}, name: 'Глеб' },
+    { id: 'anikin_antosha', balances: {}, name: 'Антон' },
+    { id: 'igor_naz', balances: {}, name: 'Игорь' },
+    { id: 'tg-marina', balances: {}, name: 'Марина' },
+    { id: 'tg-sasha', balances: {}, name: 'Саша' },
+    { id: 'tg-alena', balances: {}, name: 'Алена' },
+    { id: 'tg-serega', balances: {}, name: 'Сережа' },
+])
